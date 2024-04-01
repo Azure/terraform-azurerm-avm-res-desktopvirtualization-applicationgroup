d127c66a9d61ccc8e8112a3677e502dffee2111c
Jen Sheerin <jensheerin@microsoft.com>
2024-04-01 15:21:03 -0400

    recreate main

---
+++ examples/default/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0, < 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This picks a random region from the list of regions.
+resource "random_integer" "region_index" {
+  max = length(local.azure_regions) - 1
+  min = 0
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = local.azure_regions[random_integer.region_index.result]
+  name     = module.naming.resource_group.name_unique
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.log_analytics_workspace.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+module "avm_res_desktopvirtualization_hostpool" {
+  source                                        = "Azure/avm-res-desktopvirtualization-hostpool/azurerm"
+  version                                       = "0.1.3"
+  virtual_desktop_host_pool_resource_group_name = azurerm_resource_group.this.name
+  virtual_desktop_host_pool_name                = var.host_pool
+  virtual_desktop_host_pool_location            = azurerm_resource_group.this.location
+  virtual_desktop_host_pool_load_balancer_type  = "BreadthFirst"
+  virtual_desktop_host_pool_type                = "Pooled"
+  resource_group_name                           = azurerm_resource_group.this.name
+  diagnostic_settings = {
+    to_law = {
+      name                  = "to-law"
+      workspace_resource_id = azurerm_log_analytics_workspace.this.id
+    }
+  }
+}
+
+# This is the module desktop application group
+module "appgroup" {
+  source                                                         = "../../"
+  enable_telemetry                                               = var.enable_telemetry
+  virtual_desktop_application_group_default_desktop_display_name = var.virtual_desktop_application_group_default_desktop_display_name
+  virtual_desktop_application_group_description                  = var.virtual_desktop_application_group_description
+  virtual_desktop_application_group_friendly_name                = var.virtual_desktop_application_group_friendly_name
+  virtual_desktop_application_group_host_pool_id                 = module.avm_res_desktopvirtualization_hostpool.azure_virtual_desktop_host_pool_id
+  virtual_desktop_application_group_location                     = azurerm_resource_group.this.location
+  virtual_desktop_application_group_resource_group_name          = azurerm_resource_group.this.name
+  virtual_desktop_application_group_name                         = var.virtual_desktop_application_group_name
+  virtual_desktop_application_group_type                         = var.virtual_desktop_application_group_type
+  user_group_name                                                = var.user_group_name
+}
--- main.tf
+++ main.tf
@@ -21,6 +21,30 @@ resource "azurerm_virtual_desktop_application_group" "this" {
   }
 }
 
+# Get an existing built-in role definition
+data "azurerm_role_definition" "role" {
+  name = "Desktop Virtualization User"
+}
+
+data "azuread_groups" "existing" {
+  count = var.user_group_name == null ? 1 : 0
+
+  display_names = [var.user_group_name]
+}
+
+resource "azuread_group" "new" {
+  count = length(local.existing_group) > 0 ? 0 : 1
+
+  display_name     = var.user_group_name
+  security_enabled = true
+}
+
+resource "azurerm_role_assignment" "role" {
+  principal_id       = local.group_id
+  scope              = azurerm_virtual_desktop_application_group.this.id
+  role_definition_id = data.azurerm_role_definition.role.id
+}
+
 # Create Diagnostic Settings for AVD application group
 resource "azurerm_monitor_diagnostic_setting" "this" {
   for_each = var.diagnostic_settings
