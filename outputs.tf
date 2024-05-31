77a022477ab75f44bfa090bbe0694ebe26bfbed6
Jen Sheerin <jensheerin@microsoft.com>
2024-05-31 15:40:53 -0400

    lint fix

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "resource" {
   description = "This output is the full output for the resource to allow flexibility to reference all possible values for the resource. Example usage: module.<modulename>.resource.id"
   value       = azurerm_virtual_desktop_application_group.this
 }
+
+output "resource_id" {
+  value       = azurerm_virtual_desktop_application_group.this.id
+  description = "The ID of the Azure Virtual Desktop application group"
+}
